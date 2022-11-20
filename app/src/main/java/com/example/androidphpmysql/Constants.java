ae2b4f52b0cc7a04592d6882efb27ffebffa721b
bintangkhd <bintangkhadafi2772@gmail.com>
2022-11-20 18:29:28 +0700

    Project Finished

---
+++ app/src/main/java/com/example/androidphpmysql/Constants.java
@@ -0,0 +1,9 @@
+package com.example.androidphpmysql;
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.100.198/android-database/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL + "userLogin.php";
+}
